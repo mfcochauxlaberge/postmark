40532868dc6ad0aa491d44dcccd1763c96a34010
Marc-François Cochaux-Laberge <dev@mfcl.io>
2021-01-03 14:34:03 -0500

    Include template data in Email and marshal properly

--- email.go
+++ email.go
@@ -1,17 +1,61 @@
 package postmark
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 // Email ...
 type Email struct {
 	From          string
 	To            string
-	Subject       string `json:",omitempty"`
-	TextBody      string
-	HTMLBody      string `json:"HtmlBody"`
 	MessageStream string
 	Tag           string            `json:",omitempty"`
 	Headers       map[string]string `json:",omitempty"`
+
+	// Content
+	Subject  string `json:",omitempty"`
+	TextBody string
+	HTMLBody string `json:"HtmlBody"`
+
+	// Template
+	TemplateID    uint
+	TemplateAlias string
+	TemplateModel map[string]interface{}
+	InlineCSS     bool
+}
+
+// MarshalJSON ...
+func (e Email) MarshalJSON() ([]byte, error) {
+	email := map[string]interface{}{}
+
+	// Common fields
+	email["From"] = e.From
+	email["To"] = e.To
+	email["MessageStream"] = e.MessageStream
+	email["Tag"] = e.Tag
+	email["Headers"] = e.Headers
+
+	if e.Subject != "" {
+		// Email content
+		email["Subject"] = e.Subject
+		email["TextBody"] = e.TextBody
+		email["HtmlBody"] = e.HTMLBody
+	} else if e.TemplateID != 0 {
+		// Template
+		email["TemplateID"] = e.TemplateID
+		email["TemplateAlias"] = e.TemplateAlias
+		email["TemplateModel"] = e.TemplateModel
+		email["TemplateModel"] = e.TemplateModel
+		email["InlineCss"] = e.InlineCSS
+	}
+
+	data, err := json.Marshal(email)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
 
 // Response ...
